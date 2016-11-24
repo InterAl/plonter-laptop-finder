9e24af7f20498758c479ea5b620863d0985966c1
Alon Nachshon <alonn@wix.com>
2016-11-24 23:12:11 +0200

    bable-polyfill - on top

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
+import 'babel-polyfill';
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import configureStore from './stores';
 import App from './containers/App';
-import 'babel-polyfill';
 import 'whatwg-fetch';
-
-require('es6-object-assign').polyfill();
 
 const store = configureStore();
 
