1da82fc911229734763e63da06f04a84ecdf1c64
Alon Nachshon <alonn@wix.com>
2017-09-09 12:07:50 +0300

    images - relative urls

--- src/config/base.js
+++ src/config/base.js
@@ -1,6 +1,6 @@
 // Settings configured here will be merged into the final config object.
 export default {
-    imageUrl: 'http://www.plonter.co.il/graphics/product_images/100x100/{{filename}}',
-    productLink: 'http://www.plonter.co.il/detail.tmpl?sku={{sku}}',
-    defaultImageUrl: 'http://www.plonter.co.il/abc/graphics/logo_{{mfg}}.gif'
+    imageUrl: '/graphics/product_images/100x100/{{filename}}',
+    productLink: '/detail.tmpl?sku={{sku}}',
+    defaultImageUrl: '/abc/graphics/logo_{{mfg}}.gif'
 };
