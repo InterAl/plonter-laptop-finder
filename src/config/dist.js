dfceb5a90f2b1fc364be32788c5dcd78d73c7d8d
Alon Nachshon <alonn@wix.com>
2018-06-21 19:00:22 +0300

    alonDTlp

--- src/config/dist.js
+++ src/config/dist.js
@@ -3,7 +3,7 @@ import baseConfig from './base';
 const config = {
   appEnv: 'dist',
   laptopsUrl: '/pnp/alonLP.tmpl',
-  filtersUrl: '/pnp/alondtlp.tmpl',
+  filtersUrl: '/pnp/alonDTlp.tmpl',
   productLink: 'http://www.plonter.co.il/detail.tmpl?sku={{sku}}'
 };
 
